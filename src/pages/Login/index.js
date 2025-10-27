80c4eb7c78b8c0e3f18101f2bc2b975800ca233f
David Tsui <david0988010890david@gmail.com>
2025-10-27 16:24:09 -0400

    update async logic

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,15 @@
 import './index.scss'
 import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
+import { useDispatch } from 'react-redux'
+import { fetchLogin } from '@/store/modules/user'
 
 const Login = () => {
+    const dispatch = useDispatch()
     const onFinish = (values) =>{
         console.log(values)
+        // 觸發異步action 
+        dispatch(fetchLogin(values))
     }
     return (
         <div className="login">
@@ -12,29 +17,29 @@ const Login = () => {
             <img className="login-logo" src={logo} alt="" />
             {/* 登錄表單 */}
             <Form onFinish={onFinish} validateTrigger="onBlur">
-            <Form.Item name="mobile"
-                rules={[
-                    { 
-                        required: true, 
-                        message: 'Your phone number is wrong' 
-                    },
-                    { 
-                        pattern: /^09\d{8}$/,
-                        message: 'Your phone number is wrong' 
-                    }
-                ]}>
-                <Input size="large" placeholder="Please input your phone number!" />
-            </Form.Item>
-            <Form.Item name="code"
-                rules={[{ required: true, message: 'Your code is wrong' }]}
-                >
-                <Input size="large" placeholder="Please input your code!" />
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit" size="large" block>
-                登錄
-                </Button>
-            </Form.Item>
+                <Form.Item name="mobile"
+                    rules={[
+                        { 
+                            required: true, 
+                            message: 'Your phone number is wrong' 
+                        },
+                        { 
+                            pattern: /^1[3-9]\d{9}$/,
+                            message: 'Your phone number is wrong' 
+                        }
+                    ]}>
+                    <Input size="large" placeholder="Please input your phone number!" />
+                </Form.Item>
+                <Form.Item name="code"
+                    rules={[{ required: true, message: 'Your code is wrong' }]}
+                    >
+                    <Input size="large" placeholder="Please input your code!" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" size="large" block>
+                    登錄
+                    </Button>
+                </Form.Item>
             </Form>
         </Card>
         </div>
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,7 @@
 // 用戶相關狀態管理
 
 import { createSlice } from "@reduxjs/toolkit";
+import { request } from "@/utils";
 
 const userStore = createSlice({
     name: 'user',
@@ -21,6 +22,16 @@ const { setToken } = userStore.actions
 
 const userReducer = userStore.reducer
 
-export { setToken }
+// 異步方法
+const fetchLogin = (loginForm) => {
+    return async (dispatch) => {
+        // 發送異步請求
+        const res = await request.post('/authorizations', loginForm)
+        // 提交同步action進行token存入
+        dispatch(setToken(res.data.token))
+    }
+}
+
+export { fetchLogin, setToken }
 
 export default userReducer

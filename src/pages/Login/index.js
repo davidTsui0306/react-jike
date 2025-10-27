6eb0476a2d89e8c5dcca883c905bf7ef6d791baa
David Tsui <david0988010890david@gmail.com>
2025-10-27 14:47:36 -0400

    update login, image part

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,29 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-    return <div>this is Login</div>
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登錄表單 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="請輸入手機號" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="請輸入驗證碼" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登錄
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login

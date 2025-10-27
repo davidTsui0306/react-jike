ed1c6bfb8aeeca680356ff6e25e6bde31e24417d
David Tsui <david0988010890david@gmail.com>
2025-10-27 15:51:07 -0400

    update axois logic

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,39 +3,42 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
-  return (
-    <div className="login">
-      <Card className="login-container">
-        <img className="login-logo" src={logo} alt="" />
-        {/* 登錄表單 */}
-        <Form validateTrigger="onBlur">
-          <Form.Item name="mobile"
-            rules={[
-                { 
-                    required: true, 
-                    message: 'Your phone number is wrong' 
-                },
-                { 
-                    pattern: /^09\d{8}$/,
-                    message: 'Your phone number is wrong' 
-                }
-            ]}>
-            <Input size="large" placeholder="Please input your phone number!" />
-          </Form.Item>
-          <Form.Item name="code"
-            rules={[{ required: true, message: 'Your code is wrong' }]}
-            >
-            <Input size="large" placeholder="Please input your code!" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" size="large" block>
-              登錄
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
-  )
+    const onFinish = (values) =>{
+        console.log(values)
+    }
+    return (
+        <div className="login">
+        <Card className="login-container">
+            <img className="login-logo" src={logo} alt="" />
+            {/* 登錄表單 */}
+            <Form onFinish={onFinish} validateTrigger="onBlur">
+            <Form.Item name="mobile"
+                rules={[
+                    { 
+                        required: true, 
+                        message: 'Your phone number is wrong' 
+                    },
+                    { 
+                        pattern: /^09\d{8}$/,
+                        message: 'Your phone number is wrong' 
+                    }
+                ]}>
+                <Input size="large" placeholder="Please input your phone number!" />
+            </Form.Item>
+            <Form.Item name="code"
+                rules={[{ required: true, message: 'Your code is wrong' }]}
+                >
+                <Input size="large" placeholder="Please input your code!" />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" size="large" block>
+                登錄
+                </Button>
+            </Form.Item>
+            </Form>
+        </Card>
+        </div>
+    )
 }
 
 export default Login
---
+++ src/utils/request.js
@@ -0,0 +1,31 @@
+// axois封裝
+import axios from "axios";
+
+// 1.根域名配置
+// 2.超時時間
+// 3.請求/響應攔截器
+
+const request = axios.create({
+    baseURL: 'http://geek.itheima.net/v1_0',
+    timeout: 5000
+})
+
+// 請求攔截器
+// 請求發送前攔截，插入自定義配置
+request.interceptors.request.use((config)=> {
+    return config
+  }, (error)=> {
+    return Promise.reject(error)
+})
+
+// 響應攔截器
+// 響應返回客戶端之前攔截，處理返回數據
+request.interceptors.response.use((response)=> {
+
+    return response.data
+  }, (error)=> {
+
+    return Promise.reject(error)
+})
+
+export { request }

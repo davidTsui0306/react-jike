8f99696bbe6a08c476315efb803434700aafc23d
David Tsui <david0988010890david@gmail.com>
2025-10-27 20:11:18 -0400

    update authroute logic

---
+++ src/components/AuthRoute.js
@@ -0,0 +1,14 @@
+// 有token正常跳轉 無token回登陸
+
+import { getToken } from "@/utils"
+import { Navigate } from "react-router-dom"
+
+export function AuthRoute({ children }) {
+    const token = getToken()
+    if(token) {
+        return <>{children}</>
+    }
+    else {
+        return <Navigate to={'/login'} replace/>
+    }
+}

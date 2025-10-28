a0fada8cb595ba6f200a60916892cd84c97c637f
David Tsui <david0988010890david@gmail.com>
2025-10-27 23:56:24 -0400

    update home logic

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,37 @@
+import * as echarts from 'echarts';
+import { useEffect } from 'react';
+
+
+
 const Home = () => {
-    return <div>this is home</div>
+    useEffect(() => {
+        //保證dom可用才進行渲染
+        // 獲取渲染圖表的dom節點
+        const chartDom = document.getElementById('main');
+        // 圖表初始化生成實力對象
+        const myChart = echarts.init(chartDom);
+    
+        const option = {
+        xAxis: {
+            type: 'category',
+            data: ['Vue', 'React', 'Angular']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+            data: [10, 40, 70],
+            type: 'bar'
+            }
+        ]
+        };
+
+        // 使用圖表參數完成圖表渲染
+        option && myChart.setOption(option);
+    }, [])
+
+    return <div><div id='main' style={{width: '500px', height: '400px'}}></div></div>
 }
 
 export default Home
